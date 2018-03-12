9df5e0467b9f6a2ca56964a036699ff763a78888
amozalev <mozalev.alexey@gmail.com>
2018-03-12 12:24:11 +0300

    README.md is created.

--- manage.py
+++ manage.py
@@ -1,16 +1,12 @@
 from wsgiref.simple_server import make_server
 from app.myapp import MyApp
 
+app = MyApp()
 
-def run():
-    app = MyApp()
+if __name__ == '__main__':
     try:
         srv = make_server('localhost', 8000, app)
         print("--- Start app on port 8000 ---")
         srv.serve_forever()
     except KeyboardInterrupt:
         print('--- Stop app ---')
-
-
-if __name__ == '__main__':
-    run()
